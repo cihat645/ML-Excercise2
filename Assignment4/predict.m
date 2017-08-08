d821058b46e6813aee649129a6dca9dd6fa30329
Thomas Ciha <thomasciha111@gmail.com>
2017-08-08 17:03:00 -0300

    Add files via upload

---
+++ Assignment4/predict.m
@@ -0,0 +1,24 @@
+function p = predictNN(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+h1 = sigmoid([ones(m, 1) X] * Theta1');
+h2 = sigmoid([ones(m, 1) h1] * Theta2');
+printf("size of h1 and h2 respectively:\n");
+size(h1)
+size(h2)
+
+[dummy, p] = max(h2, [], 2); %p is storing the index of the maximum probability
+
+% =========================================================================
+
+
+end
